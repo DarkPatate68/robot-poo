76d3a1c33df0c01283f27730a816633083b4c9ce
Simeon <simeoncapy@gmail.com>
2014-06-29 20:12:42 +0200

    Doxygen SNCF
    Ajout de commentaire Doxygen dans le train

--- Library/Entities/Commentaire.class.php
+++ Library/Entities/Commentaire.class.php
@@ -3,19 +3,23 @@ namespace Library\Entities;
  
 class Commentaire extends \Library\Entity
 {
-	protected $news,
-			  $auteur,
-			  $contenu,
-			  $dateAjout,
-			  $editeur,
-			  $dateModif,
-			  $moderation,
-			  $supprime;
+	protected $news, /**< (int) Id de la news auquel se rapporte le commentaire */ 
+			  $auteur, /**< (\Library\Entities\Membre) Auteur du commentaire */
+			  $contenu, /**< (string) Contenu du commentaire */
+			  $dateAjout, /**< (\DateTime) date de création du commentaire */
+			  $editeur, /**< (\Library\Entities\Membre) Dernier éditeur du commentaire */
+			  $dateModif, /**< (\DateTime) date de la dernière modification du commentaire */
+			  $moderation, /**< (string) Message de modération */
+			  $supprime; /**< (bool) Si le message est supprimé (le message de modération est alors affiché) ou non */
    
-	const AUTEUR_INVALIDE = 1;
-	const CONTENU_INVALIDE = 2;
-	const MODERATION_INVALIDE = 3;
+	const AUTEUR_INVALIDE = 1; /**< Code renvoyé si l'auteur est invalide (mauvais type) */
+	const CONTENU_INVALIDE = 2; /**< Code renvoyé si le contenu est invalide (pas une chaîne de caractère) */
+	const MODERATION_INVALIDE = 3; /**< Code renvoyé si le message de modération est invalide (pas une chaîne de caractère) */
 
+	/**
+	 * Renvoie vrai si le commentaire est valide, faux sinon.
+	 * @return boolean
+	 */
 	public function estValide()
 	{
 		return !(empty($this->auteur) || empty($this->contenu));
@@ -23,16 +27,31 @@ class Commentaire extends \Library\Entity
 
 	// SETTERS
 
+	/**
+	 * Accesseur en écriture de l'ID de la news.
+	 * @param int $news
+	 * @return void
+	 */
 	public function setNews($news)
 	{
 		$this->news = (int) $news;
 	}
 
+	/**
+	 * Accesseur en écriture du membre
+	 * @param \Library\Entities\Membre $auteur
+	 * @return void
+	 */
 	public function setAuteur(\Library\Entities\Membre $auteur)
 	{
 		$this->auteur = $auteur;
 	}
 
+	/**
+	 * Acesseur en écriture du contenu. Ajoute self::CONTENU_INVALIDE à la variable self::erreurs si ce n'est pas un @b string
+	 * @param string $contenu
+	 * @return void
+	 */
 	public function setContenu($contenu)
 	{
 		if (!is_string($contenu) || empty($contenu))
@@ -45,11 +64,21 @@ class Commentaire extends \Library\Entity
 		}
 	}
 
+	/**
+	 * Accesseur en écriture de la date d'ajout du commentaire
+	 * @param \DateTime $date
+	 * @return void
+	 */
 	public function setDateAjout(\DateTime $date)
 	{
 		$this->dateAjout = $date;
 	}
 	
+	/**
+	 * Accesseur en écriture du dernier éditeur du commentaire
+	 * @param \Library\Entities\Membre $editeur
+	 * @return void
+	 */
 	public function setEditeur(\Library\Entities\Membre $editeur)
 	{
 		$this->editeur = $editeur;
--- Library/Validator.class.php
+++ Library/Validator.class.php
@@ -1,10 +1,20 @@
 <?php
 namespace Library;
 
+/**
+ * Classe de base de tous les validateurs, definie le seul attribut commun (errorMessage) ainsi que ses accesseurs.
+ * @author Siméon
+ * @date 29/06/2014
+ *
+ */
 abstract class Validator
 {
-	protected $errorMessage;
+	protected $errorMessage; /**< Si la condition du validateur n'est pas rempli, le message d'erreur à afficher y est stocké */
 
+	/**
+	 * Constructeur de la classe, remplie la variable errorMessage
+	 * @param string $errorMessage
+	 */
 	public function __construct($errorMessage)
 	{
 		$this->setErrorMessage($errorMessage);
@@ -12,6 +22,10 @@ abstract class Validator
 
 	abstract public function isValid($value);
 
+	/**
+	 * Définie l'accesseur en écriture de la classe abstraite pour ses classes filles
+	 * @param string $errorMessage Si le paramètre n'est pas une chaîne de caractère, rien ne sera enregistré
+	 */
 	public function setErrorMessage($errorMessage)
 	{
 		if (is_string($errorMessage))
@@ -20,6 +34,10 @@ abstract class Validator
 		}
 	}
 
+	/**
+	 * Accesseur en lecture de la variable errorMessage
+	 * @return string
+	 */
 	public function errorMessage()
 	{
 		return $this->errorMessage;
